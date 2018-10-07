5cd80bbab838ec32a2625704b46261bc8c777136
diaminho <dmitriy.pichugin.1995@gmail.com>
2018-10-07 15:42:12 +0700

    Improved Feistel Cipher

--- src/sample/FeistelСipher.java
+++ src/sample/FeistelСipher.java
@@ -1,14 +1,25 @@
 package sample;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FeistelСipher {
     private int rounds;
-
+    private int blockSize;
 
     public FeistelСipher(){
 
+    }
+
+    public int getBlockSize() {
+        return blockSize;
+    }
+
+    public void setBlockSize(int blockSize) {
+        this.blockSize = blockSize;
     }
 
     public int getRounds() {
@@ -32,69 +43,137 @@ public class FeistelСipher {
         else return "None";
     }
 
-        public void feist(int[] a, boolean reverse)
+    public Long[] doFeist(Long[] a, boolean reverse)
+    {
+        int round = reverse? rounds: 1;
+        Long[] res=new Long[2];
+        res[0] = a[0];
+        res[1] = a[1];
+        for (int i = 0; i < rounds; i++)
         {
-            int round = reverse? rounds: 1;
-            int l = a[0];
-            int r = a[1];
-            for (int i = 0; i < rounds; i++)
+            if (i < rounds-1) // если не последний раунд
             {
-                if (i < rounds-1) // если не последний раунд
-                {
-                    int t = l;
-                    l = r ^ func(l, round);
-                    r = t;
-                }
-                else // последний раунд
-                {
-                    r = r ^ func(l, round);
-                }
-                round += reverse? -1: 1;
+                Long t = res[0];
+                res[0] = res[1] ^ func(res[0], round);
+                res[1] = t;
             }
-            a[0] = l;
-            a[1] = r;
+            else // последний раунд
+            {
+                res[1] = res[1] ^ func(res[0], round);
+            }
+            round += reverse? -1: 1;
+        }
+        return res;
+    }
+
+
+    private Long func(Long b, int k) {
+        return b + k;
+    }
+
+
+    public void test() {
+        Long[] a = new Long[2];
+        a[0] = Long.valueOf(10000000);
+        a[1] = Long.valueOf(200);
+        doFeist(a, false);
+        System.out.println("a0: "+a[0]+" a1: "+a[1]);
+        doFeist(a, true);
+        System.out.println("a0: "+a[0]+" a1: "+a[1]);
+    }
+
+
+    public String getInfoFromFile(String filePath){
+        String tmpStr;
+        String infoString="";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            //чтение построчно
+            while ((tmpStr = br.readLine()) != null) {
+                infoString += tmpStr+"\n";
+            }
+            System.out.println("Исходный текст: "+infoString);
+            return infoString;
+            //System.out.println((int)'\n');
+        }
+        catch (IOException e){
+            System.out.println(e);
+            return "-1";
         }
 
+    }
 
-        private int func(int b, int k)
-        {
-            return b + k;
+    public String[] getBlockInfoBin(String info){
+        int blockCount=(int)Math.ceil((double)info.length()/blockSize);
+        //System.out.println(info.length());
+        String[] blockInfo=new String[blockCount];
+        for (int i=0;i<blockInfo.length;i++){
+            blockInfo[i]="";
+        }
+        int k=0;
+        for (int i=0;i<info.length();i++){
+            if (i==(k+1)*blockSize){
+                k++;
+            }
+            blockInfo[k]+=info.charAt(i);
+            //System.out.println(blockInfo[k]);
         }
 
+        String[] blockInfoBin=new String[blockInfo.length];
 
-        public void test()
-        {
-            int[] a = new int[2];
-            a[0] = 10000000;
-            a[1] = 200;
-            feist(a, false);
-            System.out.println("a0: "+a[0]+" a1: "+a[1]);
-            feist(a, true);
-            System.out.println("a0: "+a[0]+" a1: "+a[1]);
+        for (int i=0;i<blockInfoBin.length;i++){
+            blockInfoBin[i]="";
         }
 
-
-        public void getBinInfoFromFile(String filePath){
-            int[] info=new int[2];
-
-
+        for (int i=0;i<blockInfoBin.length;i++){
+            for (int j=0;j<blockSize;j++) {
+                //System.out.println(blockInfoBin[i].length());
+                if(j>=blockInfo[i].length()){
+                    blockInfoBin[i]+="11111111111";
+                }
+                else {
+                    blockInfoBin[i]+= asBitString(Integer.toBinaryString((int) blockInfo[i].charAt(j)), 11);
+                }
+                //System.out.println(blockInfoBin[i].length());
+            }
         }
 
-    public int[] binToDec(int[] bin) {
-        int[] dec=new int[bin.length];
-        for (int i=0;i<dec.length;i++)
-            dec[i]=Integer.parseInt(bin[i]+"",2);
-        return dec;
+        return blockInfoBin;
+    }
+
+    public String[] getLeftRightFromBlock(String block){
+        String[] blockLR=new String[2];
+        for (int i=0;i<blockLR.length;i++){
+            blockLR[i]=block.substring(i*block.length()/2,(i+1)*block.length()/2);
+        }
+        return blockLR;
+    }
+
+    public Long[] getNumberFromBlockLR(String[] blockLR){
+        Long[] numberLR=new Long[2];
+        for (int i=0;i<numberLR.length;i++) {
+            numberLR[i]=Long.parseLong(blockLR[i],2);
+        }
+        return numberLR;
     }
 
     public String asBitString(String value, int stringSize) {
         String str="";
         for (int i = 0; i < stringSize-value.length(); i++) {
-            str+='0';
+            str+="0";
         }
+
+        //System.out.println("ASBIT length: "+(str+value));
         return str+value;
     }
 
 
+    public String getStringFromBinary(String binStr){
+        String strText="";
+        //System.out.println(binStr.length());
+        for (int i=0;i<binStr.length();i+=11){
+            strText+=(char)Integer.parseInt(binStr.substring(i,i+11),2);
+        }
+        return strText;
 
+    }
 }
--- src/sample/managers/EncryptManager.java
+++ src/sample/managers/EncryptManager.java
@@ -5,9 +5,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import sample.FeistelСipher;
 
-import javax.swing.*;
-import java.io.File;
-
 public class EncryptManager {
     private static Parent root;
     FeistelСipher feistelСipher;
@@ -29,11 +26,84 @@ public class EncryptManager {
     @FXML
     public void onChooseFile(){
         feistelСipher=new FeistelСipher();
-        feistelСipher.getFile();
+        String fileName=feistelСipher.getFile();
+        feistelСipher.setRounds(3);
+        feistelСipher.setBlockSize(8);
+        feistelСipher.test();
+        String info=feistelСipher.getInfoFromFile(fileName);
+        //System.out.println("info "+info);
+        String[] blockInfo=feistelСipher.getBlockInfoBin(info);
+        for(int i=0;i<blockInfo.length;i++){
+            System.out.println("BLOCK " + i+" "+blockInfo[i]+" length "+blockInfo[i].length());
+        }
 
-        FeistelСipher feistel=new FeistelСipher();
-        feistel.setRounds(3);
-        feistel.test();
+        String[][] blocksLR=new String[blockInfo.length][2];
+        for (int i=0;i<blocksLR.length;i++){
+            blocksLR[i]=feistelСipher.getLeftRightFromBlock(blockInfo[i]);
+        }
+
+        for (int i=0;i<blocksLR.length;i++){
+            System.out.println("BLOCK: "+i);
+            for (int j=0;j<blocksLR[i].length;j++){
+                System.out.print(blocksLR[i][j]+" | ");
+            }
+            System.out.println();
+        }
+
+        Long [][] numbersLR=new Long[blocksLR.length][2];
+        for (int i=0;i<numbersLR.length;i++){
+            numbersLR[i]=feistelСipher.getNumberFromBlockLR(blocksLR[i]);
+        }
+
+        for (int i=0;i<numbersLR.length;i++){
+            System.out.println("Block: "+i);
+            for(int j=0;j<numbersLR[i].length;j++){
+                System.out.print(numbersLR[i][j]+" ");
+            }
+        }
+
+        Long[][] encryptedNumbersLR=new Long[numbersLR.length][2];
+        for (int i=0;i<encryptedNumbersLR.length;i++) {
+            encryptedNumbersLR[i]=feistelСipher.doFeist(numbersLR[i], false);
+        }
+
+        System.out.println("зашифр");
+        for (int i=0;i<encryptedNumbersLR.length;i++) {
+            System.out.println("BLOCK: "+i+" L: "+encryptedNumbersLR[i][0]+" R: "+encryptedNumbersLR[i][1]);
+        }
+
+        String[] encrBinStr=new String[encryptedNumbersLR.length];
+        for (int i=0;i<encryptedNumbersLR.length;i++) {
+            encrBinStr[i]=feistelСipher.asBitString(Long.toBinaryString(encryptedNumbersLR[i][0]),11*feistelСipher.getBlockSize()/2)+feistelСipher.asBitString(Long.toBinaryString(encryptedNumbersLR[i][1]),11*feistelСipher.getBlockSize()/2);
+            System.out.println(encrBinStr[i]);
+        }
+
+        String[] encStr=new String[encrBinStr.length];
+        for (int i=0;i<encStr.length;i++) {
+            encStr[i]=feistelСipher.getStringFromBinary(encrBinStr[i]);
+            System.out.print(encStr[i]);
+        }
+
+        //ДЕШИФРОВАНИЕ
+        System.out.println("ДЕШИФРОВАНИЕ");
+        Long[][] decryptedNumbersLR=new Long[encryptedNumbersLR.length][2];
+        for (int i=0;i<decryptedNumbersLR.length;i++) {
+            decryptedNumbersLR[i]=feistelСipher.doFeist(encryptedNumbersLR[i], true);
+        }
+
+        String[] decrBinStr=new String[decryptedNumbersLR.length];
+        for (int i=0;i<decryptedNumbersLR.length;i++) {
+            decrBinStr[i]=feistelСipher.asBitString(Long.toBinaryString(decryptedNumbersLR[i][0]),11*feistelСipher.getBlockSize()/2)+feistelСipher.asBitString(Long.toBinaryString(decryptedNumbersLR[i][1]),11*feistelСipher.getBlockSize()/2);
+            System.out.println(encrBinStr[i]);
+        }
+
+        String[] decStr=new String[decrBinStr.length];
+        for (int i=0;i<decStr.length;i++) {
+            decStr[i]=feistelСipher.getStringFromBinary(decrBinStr[i]);
+            System.out.print(decStr[i]);
+        }
+
+
 
     }
 
